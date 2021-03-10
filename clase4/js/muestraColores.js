a019d1f1cbde64604f38710ba70974c2bb4ab8a6
Marcos Pinardi <exegeses@gmail.com>
2021-03-10 12:03:45 -0300

    ejemplo muestra de colores

---
+++ clase4/js/muestraColores.js
@@ -0,0 +1,13 @@
+    //ubicamos elemento dentro del DOM
+
+    //colecciones de colores
+    var colores = [
+                    'rojo', 'naranja', 'verde',
+                    'rosa', 'azul', 'dorado',
+                    'amarillo', 'violeta', 'turquesa'
+                   ];
+    var codigos = [
+                    '#d00', '#f60', '#0d0',
+                    '#e99', '#00d', '#DAA520',
+                    '#ff0', '#e3e', '#40E0D0'
+                  ];
