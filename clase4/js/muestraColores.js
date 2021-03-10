ab07b40c9649d4a5b346f888560f755dfebbf14d
Marcos Pinardi <exegeses@gmail.com>
2021-03-10 12:28:30 -0300

    ejemplo muestra de colores resuelto

--- clase4/js/muestraColores.js
+++ clase4/js/muestraColores.js
@@ -1,4 +1,5 @@
     //ubicamos elemento dentro del DOM
+    var muestras = document.getElementById('muestras');
 
     //colecciones de colores
     var colores = [
@@ -11,3 +12,14 @@
                     '#e99', '#00d', '#DAA520',
                     '#ff0', '#e3e', '#40E0D0'
                   ];
+
+    //generación de HTML
+    var cantidad = colores.length;
+    muestras.innerHTML = '';
+    for ( var n=0; n<cantidad; n++ ) {
+        muestras.innerHTML += '<i style="background-color: '+codigos[n]+'"></i>' +colores[n]+ '<br>';
+    }
+
+    /*
+    <i style="background-color: #ccc"></i> nombre de color <br>
+    * */
