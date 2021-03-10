90b22bab0e69f01e4e3d8b977a170b80cb319da4
Marcos Pinardi <exegeses@gmail.com>
2021-03-10 12:59:38 -0300

    ejercicio muestra de fotos

---
+++ clase4/js/fotografia.js
@@ -0,0 +1,12 @@
+    var fotos = document.getElementById('fotos');
+
+    var nombres = [
+                    'arq1.jpg', 'arq2.jpg', 'arq3.jpg',
+                    'dancer1.jpg', 'dancer2.jpg', 'dancer3.jpg',
+                    'moda1.jpg', 'moda2.jpg', 'moda3.jpg'
+                   ];
+    var cantidad = nombres.length;
+    fotos.innerHTML = '';
+    for( var n = 0; n < cantidad; n++ ){
+
+    }
